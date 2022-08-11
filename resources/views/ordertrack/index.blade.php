1655343f8a54c0626f536f2d51803fb12d768f30
Md Shariful Islam <94960764+shariful94@users.noreply.github.com>
2022-08-11 17:29:18 +0600

    customer profile manage designed changed

--- resources/views/ordertrack/index.blade.php
+++ resources/views/ordertrack/index.blade.php
@@ -60,19 +60,120 @@ Order Track
             <div class="row">
                 <div class="card border-left-primary col-12 shadow mb-4">
                     <div class="card-body">
-                        Order Track
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="card border-left-primary col-12 shadow mb-4">
-                    <div class="card-body">
-                        <h1>Hello World</h1>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit porro dolor eveniet nihil quas dicta consectetur consequatur quaerat aperiam minus quos deleniti et excepturi sapiente neque doloremque ullam, in vero.
-                        Ut quisquam ad sed error ipsum fuga inventore provident cum magnam. Odio magni, quae, dicta aperiam vero tempore sequi consectetur culpa sint dolor facere non numquam minima accusantium impedit recusandae.
-                        Voluptatem expedita voluptatum amet non eos dignissimos modi cum dolore accusamus nobis? Eum, ipsa quaerat aut doloremque ullam maxime odio fuga illum modi temporibus veniam possimus amet dolor, commodi eaque!
-                        Quidem ut officia at quaerat libero iste voluptates dolores expedita. Quae distinctio doloremque aut est provident quibusdam illum quos necessitatibus dicta, tempore nobis animi quasi fugit iusto cumque sed ipsum.
-                        Nesciunt quod minus dignissimos consectetur accusamus adipisci? Tempora, accusamus. Optio vero doloremque ullam modi magni magnam, reprehenderit eaque voluptas accusamus similique dolorum alias qui fugiat nihil, dolore iste, fugit odit!</p>
+                        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+                        <div class="container">
+                          <div class="col">
+                            <div class="row">
+                              <div class="col mb-3">
+                                <div class="card">
+                                  <div class="card-body">
+                                    <div class="e-profile">
+                                      <div class="row">
+                                        <div class="col-12 col-sm-auto mb-3">
+                                          <div class="mx-auto" style="width: 140px;">
+                                            <div class="d-flex justify-content-center align-items-center rounded" style="height: 140px; background-color: rgb(233, 236, 239);">
+                                              <span style="color: rgb(166, 168, 170); font: bold 8pt Arial;">140x140</span>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
+                                          <div class="text-center text-sm-left mb-2 mb-sm-0">
+                                            <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">shariful islam</h4>
+                                            <p class="mb-0">@sharif</p>
+                                            <div class="mt-2">
+                                              <button class="btn btn-primary" type="button">
+                                                <input type="image" src="" alt="">
+                                                <i class="fa fa-fw fa-camera"></i>
+                                                <span>Change Photo</span>
+                                              </button>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="tab-content pt-3">
+                                        <div class="tab-pane active">
+                                          <form class="form" novalidate="">
+                                            <div class="row">
+                                              <div class="col">
+                                                <div class="row">
+                                                  <div class="col">
+                                                    <div class="form-group">
+                                                      <label>Full Name</label>
+                                                      <input class="form-control" type="text" name="name" placeholder="John Smith" value="John Smith">
+                                                    </div>
+                                                  </div>
+                                                  <div class="col">
+                                                    <div class="form-group">
+                                                      <label>Username</label>
+                                                      <input class="form-control" type="text" name="username" placeholder="johnny.s" value="johnny.s">
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                                <div class="row">
+                                                  <div class="col">
+                                                    <div class="form-group">
+                                                      <label>Email</label>
+                                                      <input class="form-control" type="text" placeholder="user@example.com">
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                                <div class="row">
+                                                  <div class="col mb-3">
+                                                    <div class="form-group">
+                                                      <label>About</label>
+                                                      <textarea class="form-control" rows="5" placeholder="My Bio"></textarea>
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div class="row">
+                                              <div class="col-12 col-sm-6 mb-3">
+                                                <div class="mb-2"><b>Change Password</b></div>
+                                                <div class="row">
+                                                  <div class="col">
+                                                    <div class="form-group">
+                                                      <label>Current Password</label>
+                                                      <input class="form-control" type="password" placeholder="••••••">
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                                <div class="row">
+                                                  <div class="col">
+                                                    <div class="form-group">
+                                                      <label>New Password</label>
+                                                      <input class="form-control" type="password" placeholder="••••••">
+                                                    </div>
+                                                  </div>
+                                                </div>
+                                                <div class="row">
+                                                  <div class="col">
+                                                    <div class="form-group">
+                                                      <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
+                                                      <input class="form-control" type="password" placeholder="••••••"></div>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                            <div class="row">
+                                              <div class="col d-flex justify-content-end">
+                                                <button class="btn btn-primary" type="submit">Save Changes</button>
+                                              </div>
+                                            </div>
+                                          </form>
+                                      
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                          
+                            </div>
+                        
+                          </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
